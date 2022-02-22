c64d2ee6543720260a401d61babb6b3fe19e0a08
Mahesh Bauri <www.maheshbauri@gmail.com>
2022-02-22 14:43:23 +0530

    Add Projects

---
+++ Section 3/BoxPracticeProject3.3/BoxPracticeProject3.3/App.js
@@ -0,0 +1,41 @@
+import React from "react"
+import boxes from "./boxes"
+import Box from "./Box"
+
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    function toggle(id) {
+        setSquares(prevSquares => {
+            const newSquares = []
+            for(let i = 0; i < prevSquares.length; i++) {
+                const currentSquare = prevSquares[i]
+                if(currentSquare.id === id) {
+                    const updatedSquare = {
+                        ...currentSquare,
+                        on: !currentSquare.on
+                    }
+                    newSquares.push(updatedSquare)
+                } else {
+                    newSquares.push(currentSquare)
+                }
+            }
+            return newSquares
+        })
+    }
+    
+    const squareElements = squares.map(square => (
+        <Box 
+            key={square.id} 
+            id={square.id}
+            on={square.on} 
+            toggle={toggle}
+        />
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
---
+++ Section 3/CounterPracticeProject3.6/CounterPracticeProject3.6/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Count from "./Count"
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    console.log("App component rendered")
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <Count number={count} />
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
